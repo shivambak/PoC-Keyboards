fd3f7a13d9b5ee7b0a09f0156207b0a9af225932
shivambak <shivambakshi67@gmail.com>
2024-05-17 00:16:38 -0700

    update

--- src/LegoPage.js
+++ src/LegoPage.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import './LegoPage.css';
-import legos from "./How-to-Build-Your-Lego-Collection-Gear-GettyImages-900408694.jpg.webp";
 import starwars from "./CQaJkg.png";
 import f1 from "./42171_boxprod_v39.png";
 import tower from "./GUEST_38640fdd-c8a1-4057-856d-48067eede8ee.jpeg";
@@ -13,7 +12,6 @@ function LegoPage() {
       </header>
       <main>
         <section className="main-image">
-          <img src={legos} alt="Colorful Lego Bricks" />
         </section>
         <section className="content">
           <h2>About Legos</h2>
@@ -23,7 +21,7 @@ function LegoPage() {
           <h2>Why Legos are Amazing</h2>
           <ul>
             <li>Encourages creativity and imagination</li>
-            <li>Develops fine motor skills</li>
+            <li>Develops motor skills</li>
             <li>Fun for all ages</li>
             <li>Educational and entertaining</li>
           </ul>
@@ -33,9 +31,15 @@ function LegoPage() {
           </p>
         </section>
         <section className="additional-images">
-          <img src={starwars} alt="Lego Set 1" />
-          <img src={f1} alt="Lego Set 2" />
-          <img src={tower} alt="Lego Set 3" />
+          <a href="https://www.lego.com/en-us/product/millennium-falcon-75192" target="_blank" rel="">
+            <img src={starwars} alt="Lego Set 1" />
+          </a>
+          <a href="https://www.lego.com/en-us/product/mercedes-amg-f1-w14-e-performance-42171" target="_blank" rel="">
+            <img src={f1} alt="Lego Set 2" />
+          </a>
+          <a href="https://www.lego.com/en-us/product/avengers-tower-76269" target="_blank" rel="">
+            <img src={tower} alt="Lego Set 3" />
+          </a>
         </section>
       </main>
     </div>
